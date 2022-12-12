e359b05ac457d6e3b7b95f963c0a0b9fe3790d1f
SABAH YAZILIM <SABAH YAZILIM@DESKTOP-TUMHS1A>
2022-12-12 14:12:43 +0300

    Add project files.

---
+++ MakaleWeb/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MakaleWeb.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
